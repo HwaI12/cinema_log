eb45aa580913708802b558574a17dc78875041b4
HwaI12 <s2222061@stu.musashino-u.ac.jp>
2024-04-09 10:42:11 +0900

    パッケージ.jsonとサーバーコードに変更を追加

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,8 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import app from './app';
 
-const app = express();
-const PORT = 3001;
-
-app.use(bodyParser.json());
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`)
+  console.log(`http://localhost:${PORT}`);
 });
