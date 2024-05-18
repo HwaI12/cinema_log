34b84e8486db52f64f0971a1d40ed51aea68d1f4
HwaI12 <s2222061@stu.musashino-u.ac.jp>
2024-05-18 18:08:01 +0900

    Remove unnecessary files and configurations

--- frontend/src/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://eaqujcidtcqnulirebbz.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhcXVqY2lkdGNxbnVsaXJlYmJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5MjIwNDYsImV4cCI6MjAyODQ5ODA0Nn0.g6zhcT-Z6wRQLikDPusEP1O33J6i6RmJVfsQjmU_1ho';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
